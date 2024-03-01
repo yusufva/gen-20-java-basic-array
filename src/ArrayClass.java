b7e51b7f52777e46cc592673f4c02facaebd56fb
yusufva <yusufvalent@gmail.com>
2024-03-01 14:22:15 +0700

    feature: adding Array Class

---
+++ src/ArrayClass.java
@@ -0,0 +1,46 @@
+public class ArrayClass {
+    private int[] array1;
+    private int[] newArray;
+
+    public ArrayClass(){
+        this.array1 = new int[0];
+    }
+
+    public ArrayClass(int[] a1){
+        this.array1 = a1;
+    }
+
+    public int arraySum(){
+        int sum = 0;
+        for(int num : this.array1){
+            sum+= num;
+        }
+        return sum;
+    }
+
+    public int[] twoDimArray(int[][] payload){
+        int[] sumArray = new int[payload.length];
+        for(int i=0; i< payload.length; i++){
+            int sum = 0;
+            for(int num : payload[i]){
+                sum += num;
+            }
+            sumArray[i] = sum;
+            sum = 0;
+        }
+
+        return sumArray;
+    }
+
+    public int[] transposeArray(int[] payload){
+        this.newArray = new int[payload.length];
+        for(int i=0; i<newArray.length;i++){
+            for(int j=payload.length-1; j>=0; j--){
+                newArray[i] = payload[j];
+            }
+        }
+
+        return this.newArray;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        int[] arr1 = {1,2,3};
+        ArrayClass aray = new ArrayClass();
+        System.out.println(aray.arraySum());
     }
 }
